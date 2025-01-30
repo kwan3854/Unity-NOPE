13fb46bb601bcddc12a6d3171c63ef56ed835862
Kwanjoong Lee <leekwanjung@gmail.com>
2025-01-30 21:16:25 +0900

    Add XML comments for TryGet methods
    - Added XML documentation to `TryGetValue` methods in Maybe and Result extensions.
    - Clarified return values and usage in the summaries.

--- Packages/Unity-NOPE/Runtime/Core/Maybe/MaybeExtensions.TryGet.cs
+++ Packages/Unity-NOPE/Runtime/Core/Maybe/MaybeExtensions.TryGet.cs
@@ -3,6 +3,10 @@ namespace NOPE.Runtime.Core.Maybe
 {
     public static partial class MaybeExtensions
     {
+        /// <summary>
+        /// Tries to get the value from a <see cref="Maybe{T}"/>.
+        /// </summary>
+        /// <returns>True if the Maybe has a value, false otherwise.</returns>
         public static bool TryGetValue<T>(this Maybe<T> maybe, out T value)
         {
             value = default;
--- Packages/Unity-NOPE/Runtime/Core/Result/ResultExtensions.TryGet.cs
+++ Packages/Unity-NOPE/Runtime/Core/Result/ResultExtensions.TryGet.cs
@@ -3,6 +3,10 @@ namespace NOPE.Runtime.Core.Result
 {
     public static partial class ResultExtensions
     {
+        /// <summary>
+        /// Tries to get the value from a <see cref="Result{T, E}"/>.
+        /// </summary>
+        /// <returns>True if the Result is successful, false otherwise.</returns>
         public static bool TryGetValue<T, E>(this Result<T, E> result, out T value)
         {
             value = default;
@@ -11,6 +15,10 @@ namespace NOPE.Runtime.Core.Result
             return true;
         }
         
+        /// <summary>
+        /// Tries to get the error from a <see cref="Result{T, E}"/>.
+        /// </summary>
+        /// <returns>True if the Result is failed, false otherwise.</returns>
         public static bool TryGetError<T, E>(this Result<T, E> result, out E error)
         {
             error = default;
